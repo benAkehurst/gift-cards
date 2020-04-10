860841d439fb431f73bdc8aec6ecc7c93e522942
benakehurst <benakehurst@hotmail.com>
2020-04-10 16:02:23 +0100

    feat(client): add input component

---
+++ client/gift-card/src/components/UI/Input/Input.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import classes from './Input.module.css';
+
+const input = (props) => {
+  let inputElement = null;
+  let validationError = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+  if (props.invalid && props.touched) {
+    validationError = (
+      <p className={classes.ValidationError}>
+        Please enter a valid {props.elementConfig.name}
+      </p>
+    );
+  }
+
+  switch (props.elementType) {
+    case 'input':
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'text-area':
+      inputElement = (
+        <textarea
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.displayValue}
+              </option>
+            );
+          })}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+  }
+
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+      {validationError}
+    </div>
+  );
+};
+
+export default input;
