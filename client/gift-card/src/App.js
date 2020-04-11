9da52dae6ad0743204c9d4fc6237d2841b550ff5
benakehurst <benakehurst@hotmail.com>
2020-04-11 17:02:06 +0100

    feat(client): add account route

--- client/gift-card/src/App.js
+++ client/gift-card/src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout';
 import Auth from './Containers/Auth/Auth';
 import Home from './Containers/Home/Home';
 import Admin from './Containers/Admin/Admin';
+import Account from './Containers/Account/Account';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Layout>
           <Switch>
             <Route path="/" exact component={Home}></Route>
+            <Route path="/account" component={Account}></Route>
             <Route path="/auth" component={Auth}></Route>
             <Route path="/admin" component={Admin}></Route>
           </Switch>
