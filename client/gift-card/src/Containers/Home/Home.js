2d76d8bf5fe50a69a0814bbe79ef7ed6832f7fdf
benakehurst <benakehurst@hotmail.com>
2020-04-11 16:50:31 +0100

    feat(client): scaffolding on home component

--- client/gift-card/src/Containers/Home/Home.js
+++ client/gift-card/src/Containers/Home/Home.js
@@ -6,6 +6,8 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import Header from '../../components/UI/Header/Header';
 import Banner from '../../components/UI/Banner/Banner';
 import InfoDisplay from '../../components/UI/InfoDisplay/InfoDisplay';
+import Button from '../../components/UI/Button/Button';
+import Card from '../../components/Card/Card';
 
 class Home extends Component {
   state = {};
@@ -15,9 +17,14 @@ class Home extends Component {
         <section className={classes.Header}>
           <Banner>Tasty Coffe Rewards</Banner>
           <Header userName={'Ben'}></Header>
+          <section className={classes.Card}>
+            <Card></Card>
+          </section>
         </section>
-        <section className={classes.Card}>Card</section>
-        <section className={classes.Controls}>Id & Button</section>
+        <section className={classes.Controls}>
+          <InfoDisplay>ID - AAAA</InfoDisplay>
+          <Button btnType={'General'}>Account</Button>
+        </section>
       </div>
     );
   }
