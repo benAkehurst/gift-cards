5bd9002ed2ffb647c0e42a171c5906da383099da
benakehurst <benakehurst@hotmail.com>
2020-04-13 12:04:39 +0100

    feat(app): update auth and input components

--- client/gift-card/src/Containers/Auth/Auth.js
+++ client/gift-card/src/Containers/Auth/Auth.js
@@ -3,47 +3,130 @@ import classes from './Auth.module.css';
 import axios from '../../axios-connector';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-import { getUserId } from '../../Helpers/localStorage';
-
 import Button from '../../components/UI/Button/Button';
 import Banner from '../../components/UI/Banner/Banner';
+import Input from '../../components/UI/Input/Input';
 class Auth extends Component {
   state = {
+    controls: {
+      userName: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your First Name',
+        },
+        value: '',
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email Address',
+        },
+        value: '',
+        validation: {
+          required: true,
+          isEmail: true,
+        },
+        valid: false,
+        touched: false,
+      },
+      password: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'password',
+          placeholder: 'Your Password',
+        },
+        value: '',
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+        valid: false,
+        touched: false,
+      },
+    },
     isRegister: true,
   };
 
-  componentWillMount() {
-    if (getUserId()) {
-      this.setState({
-        isRegister: false,
-      });
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderFrom = { ...this.state.orderForm };
+    const updatedFormElement = { ...updatedOrderFrom[inputIdentifier] };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validatation
+    );
+    updatedFormElement.touched = true;
+    updatedOrderFrom[inputIdentifier] = updatedFormElement;
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderFrom) {
+      formIsValid = updatedOrderFrom[inputIdentifier].valid && formIsValid;
     }
+    this.setState({ orderForm: updatedOrderFrom, formIsValid: formIsValid });
+  };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+    if (!rules) {
+      return true;
+    }
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.isEmail) {
+      const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      isValid = pattern.test(value) && isValid;
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
+    }
+    return isValid;
   }
 
-  loginHandler = () => {};
-
-  registerHandler = () => {};
-
   render() {
-    let button = (
-      <Button btnType={'General'} clicked={this.loginHandler}>
-        Login
-      </Button>
-    );
-    if (this.state.isRegister) {
-      button = (
-        <Button btnType={'General'} clicked={this.registerHandler}>
-          Register
-        </Button>
-      );
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key],
+      });
     }
+    const form = formElementsArray.map((formElement) => {
+      return (
+        <Input
+          key={formElement.id}
+          elementType={formElement.config.elementType}
+          elementConfig={formElement.config.elementConfig}
+          value={formElement.config.value}
+          invalid={!formElement.config.valid}
+          shouldValidate={formElement.config.validation}
+          touched={formElement.config.touched}
+          changed={(event) => this.inputChangedHandler(event, formElement.id)}
+        />
+      );
+    });
     return (
       <div className={classes.Auth}>
         <section className={classes.Header}>
           <Banner>Tasty Coffe Rewards</Banner>
         </section>
-        <section className={classes.FormContainer}>Form</section>
-        <section className={classes.Button}>{button}</section>
+        <section className={classes.FormContainer}>
+          <h3>Sign Up</h3>
+          <form>
+            {form}
+            <Button btnType={'General'}>Submit</Button>
+          </form>
+        </section>
       </div>
     );
   }
--- client/gift-card/src/components/UI/Input/Input.js
+++ client/gift-card/src/components/UI/Input/Input.js
@@ -1,56 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Input.module.css';
 
-class Input extends Component {
-  render() {
-    let inputElement = null;
-    let validationError = null;
-    let inputClasses = [classes.InputElement];
+const input = (props) => {
+  let inputElement = null;
+  let validationError = null;
+  const inputClasses = [classes.InputElement];
 
-    if (this.props.invalid && this.props.shouldValidate && this.props.touched) {
-      inputClasses.push(classes.Invalid);
-    }
-    if (this.props.invalid && this.props.touched) {
-      validationError = (
-        <p className={classes.ValidationError}>
-          Please enter a valid {this.props.elementConfig.name}
-        </p>
-      );
-    }
-
-    switch (this.props.elementType) {
-      case 'input':
-        inputElement = (
-          <input
-            className={inputClasses.join(' ')}
-            {...this.props.elementConfig}
-            value={this.props.value}
-            onChange={this.props.changed}
-            name={this.props.elementType}
-          />
-        );
-        break;
-      default:
-        inputElement = (
-          <input
-            className={inputClasses.join(' ')}
-            {...this.props.elementConfig}
-            value={this.props.value}
-            onChange={this.props.changed}
-          />
-        );
-        break;
-    }
-    return (
-      <div className={classes.Input}>
-        <label className={classes.Label} for={this.props.elementType}>
-          {this.props.label}
-        </label>
-        {inputElement}
-        {validationError}
-      </div>
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+  if (props.invalid && props.touched) {
+    validationError = (
+      <p className={classes.ValidationError}>
+        Please enter a valid {props.elementConfig.name}
+      </p>
     );
   }
-}
 
-export default Input;
+  switch (props.elementType) {
+    case 'input':
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'text-area':
+      inputElement = (
+        <textarea
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+    case 'select':
+      inputElement = (
+        <select
+          className={inputClasses.join(' ')}
+          value={props.value}
+          onChange={props.changed}
+        >
+          {props.elementConfig.options.map((option) => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.displayValue}
+              </option>
+            );
+          })}
+        </select>
+      );
+      break;
+    default:
+      inputElement = (
+        <input
+          className={inputClasses.join(' ')}
+          {...props.elementConfig}
+          value={props.value}
+          onChange={props.changed}
+        />
+      );
+      break;
+  }
+
+  return (
+    <div className={classes.Input}>
+      <label className={classes.Label}>{props.label}</label>
+      {inputElement}
+      {validationError}
+    </div>
+  );
+};
+
+export default input;
