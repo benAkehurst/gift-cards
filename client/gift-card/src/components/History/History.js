28f88914c6a8e3ffb3f4eb824c5a2a1134388cc7
benakehurst <benakehurst@hotmail.com>
2020-04-13 16:39:20 +0100

    feat(client): reverse order of history to display recent first

--- client/gift-card/src/components/History/History.js
+++ client/gift-card/src/components/History/History.js
@@ -15,23 +15,33 @@ class History extends Component {
     let itemsList = null;
     switch (this.props.display) {
       case 'completed':
-        itemsList = this.props.historyData.map((item) => {
-          return (
-            <li key={item.completed_date}>
-              {dateFormatter(item.completed_date)}
-            </li>
-          );
-        });
+        itemsList = this.props.historyData
+          .slice(0)
+          .reverse()
+          .map((item) => {
+            return (
+              <li key={item.completed_date}>
+                {dateFormatter(item.completed_date)}
+              </li>
+            );
+          });
         break;
       case 'stamps':
-        itemsList = this.props.historyData.map((item) => {
-          return (
-            <li key={item.created_date}>
-              {dateFormatter(item.created_date)} - {item.stamp_count}{' '}
-              {item.stamp_count > 1 ? <span>Stamps</span> : <span>Stamp</span>}
-            </li>
-          );
-        });
+        itemsList = this.props.historyData
+          .slice(0)
+          .reverse()
+          .map((item) => {
+            return (
+              <li key={item.created_date}>
+                {dateFormatter(item.created_date)} - {item.stamp_count}{' '}
+                {item.stamp_count > 1 ? (
+                  <span>Stamps</span>
+                ) : (
+                  <span>Stamp</span>
+                )}
+              </li>
+            );
+          });
         break;
       default:
         itemsList = <li>Nothing to display</li>;
