80cc5297843a491293a92536727bc8cb288bfcd0
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-02-02 17:30:20 +0000

    update auth controller

--- server/api/controllers/auth.controller.js
+++ server/api/controllers/auth.controller.js
@@ -58,13 +58,19 @@ exports.login_user = async (req, res) => {
               expiresIn: rememberMe ? '48h' : '1h',
             }
           );
-          let userFiltered = _.pick(user.toObject(), ['firstName', 'uniqueId']);
+          let userFiltered = _.pick(user.toObject(), [
+            'firstName',
+            'uniqueId',
+            'customerId',
+            'current_stamps',
+            'completed_cards',
+            'transactions',
+          ]);
           userFiltered.token = token;
           res.cookie('session', token, {
             expiresIn: rememberMe ? '48h' : '1h',
           });
-          let tasks = await Task.find({ user: user.uniqueId });
-          userFiltered.tasks = tasks;
+
           res.status(200).json({
             success: true,
             message: 'Successfully logged in',
@@ -163,6 +169,7 @@ exports.create_new_user = async (req, res) => {
         acceptedTerms: true,
         createdOnDate: format(new Date(), 'dd/MM/yyyy'),
         uniqueId: uuidv4(),
+        customerId: `_${Math.random().toString(36).substr(2, 8)}`,
       });
       const user = await newUser.save();
       const token = jwt.sign(
@@ -190,10 +197,20 @@ exports.create_new_user = async (req, res) => {
         html: `<p>Please use the following link within the next 10 minutes to activate your account on YOUR APP: <strong><a href="${baseUrl}/api/v1/auth/verification/verify-account/${user.uniqueId}/${secretCode}" target="_blank">Email Verification Link</a></strong></p>`,
       };
       await sendEmail(data);
+      let userFiltered = _.pick(user.toObject(), [
+        'firstName',
+        'email',
+        'uniqueId',
+        'customerId',
+        'current_stamps',
+        'completed_cards',
+        'transactions',
+      ]);
+      userFiltered.token = token;
       res.status(201).json({
         success: true,
         message: 'User created',
-        data: { uniqueId: user.uniqueId, token: token, tasks: null },
+        data: userFiltered,
       });
     } catch {
       res.status(400).json({
