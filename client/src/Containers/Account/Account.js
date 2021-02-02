d872794285f542569828e7b5e71454b346b83321
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-02-02 12:21:03 +0000

    make component functional

--- client/src/Containers/Account/Account.js
+++ client/src/Containers/Account/Account.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './Account.scss';
+import * as AppConfig from '../../config/AppConfig';
 import axios from '../../axios-connector';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { getUserId } from '../../Helpers/localStorage';
@@ -10,46 +11,44 @@ import Button from '../../components/UI/Button/Button';
 import History from '../../components/History/History';
 import Divider from '../../components/UI/Divider/Divider';
 
-class Account extends Component {
-  goBackHandler = () => {
-    this.props.history.goBack();
-  };
-
-  componentDidMount() {
+const Account = (props) => {
+  useEffect(() => {
     const _id = getUserId();
     if (!_id) {
-      this.props.history.push({ pathname: '/auth' });
+      props.history.push({ pathname: '/auth' });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div className="Account">
-        <section className="Header">
-          <Banner>Tasty Coffee Rewards</Banner>
-        </section>
-        <section className="HistoryContainer">
-          <History
-            historyData={this.props.location.state.transactions}
-            title={'Stamps'}
-            display={'stamps'}
-          />
-          <Divider size={'small'} />
-          <History
-            historyData={this.props.location.state.completedCards}
-            title={'Completed Cards'}
-            display={'completed'}
-          />{' '}
-        </section>
-        <section className="Controls">
-          <InfoDisplay dispStr={this.props.location.state.appId}></InfoDisplay>
-          <Button btnType={'General'} clicked={this.goBackHandler}>
-            Back
-          </Button>
-        </section>
-      </div>
-    );
-  }
-}
+  const goBackHandler = () => {
+    props.history.goBack();
+  };
+
+  return (
+    <div className="Account">
+      <section className="Header">
+        <Banner>{AppConfig.APP_NAME}</Banner>
+      </section>
+      <section className="HistoryContainer">
+        <History
+          historyData={props.location.state.transactions}
+          title="Stamps"
+          display="stamps"
+        />
+        <Divider size="small" />
+        <History
+          historyData={props.location.state.completedCards}
+          title="Completed Cards"
+          display="completed"
+        />
+      </section>
+      <section className="Controls">
+        <InfoDisplay dispStr={props.location.state.appId}></InfoDisplay>
+        <Button btnType="General" clicked={goBackHandler}>
+          Back
+        </Button>
+      </section>
+    </div>
+  );
+};
 
 export default withErrorHandler(Account, axios);
