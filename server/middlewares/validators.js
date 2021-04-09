8a51fb08d86c2638b8975e197157c1587af4bde6
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-04-09 16:47:47 +0100

    remove check token validator

--- server/middlewares/validators.js
+++ server/middlewares/validators.js
@@ -44,38 +44,9 @@ const checkUserExists = async (uuid) => {
   });
 };
 
-const checkToken = (req) => {
-  let token = req;
-  return new Promise((resolve, reject) => {
-    if (!token) {
-      reject({ success: false, message: 'No token' });
-    } else if (token) {
-      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-          reject({
-            success: false,
-            message: 'Token is not valid',
-          });
-        } else {
-          resolve({
-            success: true,
-            message: 'Token is valid',
-          });
-        }
-      });
-    } else {
-      reject({
-        success: false,
-        message: 'No Token supplied',
-      });
-    }
-  });
-};
-
 module.exports = {
   validateEmail,
   validatePassword,
   checkEmailExists,
   checkUserExists,
-  checkToken,
 };
