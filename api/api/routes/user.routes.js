b3fc1e9e09cbf00e6833d78ea5866d042d9a33d7
Ben <ben.akehurst@sainsburys.co.uk>
2021-11-15 16:37:05 +0000

    adding user controller

--- api/api/routes/user.routes.js
+++ api/api/routes/user.routes.js
@@ -1,7 +1,12 @@
-'use strict';
 module.exports = (app) => {
-  const userController = require('../controllers/user.controller');
+  const userController = require("../controllers/user.controller");
   app
-    .route('/api/v1/user/fetch-user-info/:uniqueId')
-    .get(userController.fetch_user_information);
+    .route("/api/v2/user/add-card/:token/:userId/:storeId")
+    .post(userController.add_card);
+  app
+    .route("/api/v2/user/remove-card/:token/:userId/:storeId")
+    .post(userController.remove_card);
+  app
+    .route("/api/v2/user/add-transaction/:token/:userId/:storeId/:noOfStamps")
+    .post(userController.add_transaction);
 };
