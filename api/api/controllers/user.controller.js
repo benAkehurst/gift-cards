80594d68928aade0d15dae93b7d1ccbdf3a9cfb4
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-11-16 11:35:17 +0000

    adding blank methods

--- api/api/controllers/user.controller.js
+++ api/api/controllers/user.controller.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const Store = mongoose.model('Store');
 
 exports.add_card = async (req, res) => {};
+exports.get_all_cards = async (req, res) => {};
+exports.remove_card = async (req, res) => {};
+exports.add_stamp = async (req, res) => {};
